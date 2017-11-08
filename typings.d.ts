62dd1e19f75fac9af3857f9469a21abc0416faf6
Vivek Kumar Bansal <vkb0310@gmail.com>
2017-11-08 13:35:30 +0530

    update config

--- typings.d.ts
+++ typings.d.ts
@@ -22,3 +22,23 @@ declare module 'date-fns/startOfMonth' {
     import { startOfMonth } from 'date-fns';
     export default startOfMonth;
 }
+
+declare module 'date-fns/setMonth' {
+    import { setMonth } from 'date-fns';
+    export default setMonth;
+}
+
+declare module 'date-fns/setYear' {
+    import { setYear } from 'date-fns';
+    export default setYear;
+}
+
+declare module 'date-fns/startOfWeek' {
+    import { startOfWeek } from 'date-fns';
+    export default startOfWeek;
+}
+
+declare module 'date-fns/endOfWeek' {
+    import { endOfWeek } from 'date-fns';
+    export default endOfWeek;
+}
