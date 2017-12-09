0b8de5ab7b6d2216e96386c84dad40441756a098
Vivek Kumar Bansal <vkb0310@gmail.com>
2017-12-09 09:30:48 +0530

    update configs

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,11 @@
 module.exports = {
-    setupFiles: ['<rootDir>/tests/.setup.js'],
+    setupFiles: ['<rootDir>/src/__tests__/.setup.js'],
     testMatch: ['**/?(*.)(spec|test).ts?(x)'],
     moduleFileExtensions: ['ts', 'tsx', 'js'],
-    roots: ['<rootDir>/tests'],
+    roots: ['<rootDir>/src/__tests__/'],
     transform: {
         '^.+\\.tsx?$': '<rootDir>/node_modules/ts-jest/preprocessor.js'
     },
     snapshotSerializers: ['enzyme-to-json/serializer', 'jest-glamor-react/dist/serializer'],
-    collectCoverageFrom: ['src/**/*.{ts,tsx}']
+    collectCoverageFrom: ['src/*.{ts,tsx}']
 };
